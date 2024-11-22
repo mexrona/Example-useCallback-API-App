0453502e6c5c9800df3890ba8383372f5d8dd47f
mexrona <tuichievamexrona@gmail.com>
2024-11-22 21:20:04 +0300

    useCallback example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useCallback} from "react";
+import Child from "./components/Child";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [id, setId] = useState("");
+    const [postTitle, setPostTitle] = useState("");
+    const [postBody, setPostBody] = useState("");
+
+    const updateOne = () => {
+        console.log("Я не мемоизирован");
+    };
+
+    const updateTwo = useCallback(() => {
+        console.log("Я мемоизирован!");
+    }, [postTitle, postBody]);
+
+    const getPost = async () => {
+        const response = await fetch(
+            `https://jsonplaceholder.typicode.com/posts/${id}`
+        );
+        const post = await response.json();
+
+        const title = post.title;
+        const body = post.body;
+
+        setPostTitle(title);
+        setPostBody(body);
+    };
+
+    return (
+        <div className="App">
+            <br />
+            <input
+                placeholder="ID"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+            />
+            <button onClick={getPost}>Получить пост</button>
+            <br />
+            {postTitle && (
+                <p>
+                    <b>Название:</b> {postTitle}
+                </p>
+            )}
+            {postBody && (
+                <p>
+                    <b>Текст:</b> {postBody}
+                </p>
+            )}
+            <Child updateOne={updateOne} updateTwo={updateTwo} />
+        </div>
+    );
 }
 
 export default App;
